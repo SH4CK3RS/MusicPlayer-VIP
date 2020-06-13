6391114b7eed172440030b9d5796492e110e169c
byeonggeunSon <qudrms007@sch.ac.kr>
2020-06-14 02:29:59 +0900

    feat: 2개의 IBAction추가 및 button, slider에 이벤트 연결

--- MusicPlayer/MusicPlayer/ViewController.swift
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -19,6 +19,12 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view.
   }
   
+  @IBAction func touchUpPlayPauseButton(_ sender: UIButton){
+    print("Button Tapped")
+  }
   
+  @IBAction func sliderValueChanged(_ sender: UISlider){
+    print("slider value changed")
+  }
 }
 
