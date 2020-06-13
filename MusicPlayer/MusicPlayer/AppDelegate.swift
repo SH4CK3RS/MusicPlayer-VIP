308cd07f6044c2db5d61271776aaaba354bb4b8f
byeonggeunSon <qudrms007@sch.ac.kr>
2020-06-14 01:55:16 +0900

    project: 기본 프로젝트 구성

---
+++ MusicPlayer/MusicPlayer/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  MusicPlayer
+//
+//  Created by 손병근 on 2020/06/14.
+//  Copyright © 2020 ByeonggeunSon. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 손병근 on 2020/06/14.
+//  Copyright © 2020 ByeonggeunSon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
